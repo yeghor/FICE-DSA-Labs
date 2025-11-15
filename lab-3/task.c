834407b06ade7412908a9e2cdaa55749bc2c026f
RomanovYehor <yeghor.romanov@gmail.com>
2025-11-15 15:56:18 +0200

    feat: finished lab-3 code

--- lab-3/task.c
+++ lab-3/task.c
@@ -7,69 +7,76 @@
 // Ordered matrix traversal
 
 int main() {
-
 	// Note that rows amount must be even to display all the matrix elements according to task variant
-    int rows = 6;
-    int cols = 8;
+    int rows = 8;
+    int cols = 10;
 
 	if (!((rows % 2) == 0)) {
-//		printf("%s\n", "Rows amount must be even!");
 		return 0;
 	};
 
-//	printf("%s\n", "Starting creating matrix...");
-
     // Create matrix
-    int matrix[rows][cols];
-    int matrixVal = 0;
+    char matrix[rows][cols];
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            matrix[i][j] = matrixVal;
-            matrixVal++;
+            matrix[i][j] = '.';
         };
     };
 
-//	printf("%s\n", "Matrix created");
-
 	// Start position according to variant №14
-	int i = ceil((rows-1) / 2);
-	int j = (rows-1) - i;
 
-	int normalization = (rows-1) - i;
+	// Getting bottom-central row index
+	int i = (rows) / 2;
+	int normalization = (rows - 1) - i;
+	int j = abs(normalization - (cols - 1));
 
-//	printf("%s\n", "Starting displaying matrix spiral traversal...");
-
-	while (i < rows && j < cols &&  (!(matrix[j][j] == matrix[rows-1][cols-1]))) {
+	while (1) {
 		// While loop iteration: left -> up -> right -> down
 
 		// Left
-		for (j; j > normalization; i--) {
-			usleep(100000);
-			printf("\033[%d;%dH%d", i, j, matrix[i][j]);
+		for (j; j >= normalization; j--) {
+			usleep(1000);
+			printf("\033[%d;%dH%c", i+1, j+1, matrix[i][j]);
 		};
 
+		j++;
+
 		// Up
-		for (i; i > normalization; i--) {
-			usleep(100000);
-			printf("\033[%d;%dH%d", i, j, matrix[i][j]);
+		for (i; i >= normalization; i--) {
+			usleep(1000);
+			printf("\033[%d;%dH%c", i+1, j+1, matrix[i][j]);
 		};
+
+		i++;
 
 		normalization -= 1;
 
 		// Right
-		for (j; j < (cols - 1) - normalization; j++) {
-			usleep(100000);
-			printf("\033[%d;%dH%d", i, j, matrix[i][j]);
+		for (j; j <= (cols - 1) - normalization; j++) {
+			usleep(1000);
+
+			if ( j == (cols - 1) - normalization && normalization == -1 ) {
+				return 0;
+			};
+
+			printf("\033[%d;%dH%c", i+1, j+1, matrix[i][j]);
 		};
+
+		if (normalization == -1) {
+    		return 0;
+		};
+
+		j--;
 
 		// Down
-		for (i; i > (rows-1) - normalization; i--) {
-			usleep(100000);
-			printf("\033[%d;%dH%d", i, j, matrix[i][j]);
+		for (i; i <= (rows - 1) - normalization; i++) {
+			usleep(1000);
+			printf("\033[%d;%dH%c", i+1, j+1, matrix[i][j]);
 		};
-	};
 
-//	printf("%s\n", "Matrix traversal displayed.");
+		i--;
+
+	};
 
     return 0;
 };
