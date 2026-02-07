e8e6ba00071446009b9f7a48daae4eabdda1069a
RomanovYehor <yeghor.romanov@gmail.com>
2026-02-07 15:07:38 +0200

    added: finished lab №2 part two

--- part_two/lab-2/solution.py
+++ part_two/lab-2/solution.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
+import random
+import string
 
 class Node:
     """
     Doubly connected linked list node
     """
-    def __init__(self, val: str, next: Node | None = None, prev: Node | None = None):
+    def __init__(self, val: str, next_: Node | None = None, prev: Node | None = None):
         if len(val) < 5:
             raise ValueError("Maximum value length exceeded!")
 
         self.val = val
-        self.next = next
+        self.next = next_
         self.prev = prev
     
     def __str__(self):
@@ -20,28 +22,47 @@ class Node:
 
 class LinkedList:
     @staticmethod
-    def validate_value(self, val: any) -> None:
+    def validate_value(val: any) -> None:
         """Raises: ValueError"""
-        if len(val) < 5:
+        if len(val) > 5:
             raise ValueError("Maximum value length exceeded!")
         elif not isinstance(val, str):
             raise ValueError("Invalid value type. Must be str!")
 
-    def __init__(self, head: Node):
+    def __init__(self, head: Node | None = None):
         self.__head = head
 
     def __str__(self):
         string = ""
 
         curr = self.__head
-        
+        idx = 0
         while curr:
-            string += f"{curr} -> "
+            string += f"{idx}: {curr} -> "
             curr = curr.next
+            idx += 1
         
-        string += "None"
+        if string:
+            string += "None"
+        else:
+            string = "Empty List"
 
         return string
+
+    def clear(self) -> None:
+        # Garbage collector will delete sequent nodes
+        # Since after we null head, nothing will refer to the rest nodes
+        self.__head = None
+
+    def __len__(self) -> int:
+        length = 0
+        curr = self.__head
+
+        while curr:
+            length += 1
+            curr = curr.next
+        
+        return length
 
     def reverse_in_place(self) -> None:
         if not self.__head:
@@ -58,21 +79,27 @@ class LinkedList:
 
         while curr:
             temp = curr.prev
-            curr.next = temp
+             
             curr.prev = curr.next
+            curr.next = temp
 
             curr = temp
 
 
     def append(self, val: str) -> None:
+        new_node = Node(val, None, None)
 
+        if not self.__head:
+            self.__head = new_node
+            return
 
         curr = self.__head
 
         while curr.next:
             curr = curr.next
 
-        curr.next = Node(val, None, curr)
+        new_node.prev = curr
+        curr.next = new_node
     
     def delete_by_value(self, val: str) -> bool:
         """Returns boolean value whether the node was deleted. \n\n Deletes only first matching value found"""
@@ -86,10 +113,11 @@ class LinkedList:
             if curr.val == val:
                 if not curr.prev:
                     self.__head = curr.next
+                    if self.__head:
+                        self.__head.prev = None
                     return True
                 if not curr.next:
-                    prev = curr.prev
-                    prev.next = None
+                    curr.prev.next = None
                     return True
 
                 prev = curr.prev
@@ -99,6 +127,36 @@ class LinkedList:
                 next_.prev = prev
 
                 return True
-
             else:
                 curr = curr.next
+        
+        return False
+
+if __name__ == "__main__":
+    ll = LinkedList()
+
+    n = int(input("Enter the list length (digit, greater than zero): "))
+
+    for i in range(n):
+        val = "".join([random.choice(string.ascii_lowercase) for _ in range(5)])
+        ll.append(val)
+    
+    print("Original list:")
+    print(ll)
+
+    ll.reverse_in_place()
+
+    print("Reversed list:")
+    print(ll)
+
+    while 1:
+        if len(ll) == 0:
+            break
+
+        to_delete = input("Choose value of node you want to delete (type 'exit' to finish): ")
+
+        if to_delete == "break":
+            break
+
+        ll.delete_by_value(to_delete)
+        print(ll)
